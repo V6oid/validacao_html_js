c0d5fb9a0b668e8e8b0adeb9bf5e5d42a76a2271
V6oid <141415880+V6oid@users.noreply.github.com>
2023-08-24 19:06:24 -0300

    remake total dos arquivos

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
-// Function to handle form submission
 document.getElementById('valueForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
     
-    // Get input values
     const valueA = parseFloat(document.getElementById('valueA').value);
     const valueB = parseFloat(document.getElementById('valueB').value);
 
-    // Limpe o formulário
-    
-    // Check values and display message
     if (valueB > valueA) {
-        displayMessage('Validação', 'Valor B é maior que Valor A.');
+        displayMessage('Validado', 'Valor B é maior que Valor A.');
         clearForm();
     } else if (valueA >= valueB) {
         displayMessage('Erro', 'Valor A é maior ou igual que Valor B.');
     }
 });
 
-// Function to display result message
 function displayMessage(title, message) {
     const resultMessage = document.getElementById('resultMessage');
     resultMessage.innerHTML = `<h2>${title}</h2><p>${message}</p>`;
 }
 
-// Função para limpar o formulário
 function clearForm() {
     document.getElementById('valueA').value = '';
     document.getElementById('valueB').value = '';
